03a397016507fe777d64e36351a45dbc20aba6e7
Tina-Lightning <c.atherholt@gmail.com>
2020-12-09 10:06:45 -0500

    got updating posts up and running

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -8,7 +8,7 @@ import * as moment from "moment"
 
 import useStyles from "./styles";
 
-const Post = ({ post }) => {
+const Post = ({ post, setCurrentId }) => {
     const classes = useStyles();
     return (
         <Card className={classes.card}>
@@ -18,7 +18,11 @@ const Post = ({ post }) => {
                 <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
             </div>
             <div className={classes.overlay2}>
-                <Button style={{ color: "white" }} size="small" onClick={() => { }}>
+                <Button
+                    style={{ color: "white" }}
+                    size="small"
+                    onClick={() => setCurrentId(post._id)}
+                >
                     <MoreHorizIcon font="default" />
                 </Button>
             </div>
@@ -29,12 +33,12 @@ const Post = ({ post }) => {
                 <Typography className={classes.title} variant="h5" gutterBottom>{post.message}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
-                <Button size="small" color="primary" onClick={() => {}}>
+                <Button size="small" color="primary" onClick={() => { }}>
                     <ThumbUpAltIcon fontSize="small" />
                     Like
                     {post.likeCount}
                 </Button>
-                <Button size="small" color="primary" onClick={() => {}}>
+                <Button size="small" color="primary" onClick={() => { }}>
                     <DeleteIcon fontSize="small" />
                     Delete
                 </Button>
